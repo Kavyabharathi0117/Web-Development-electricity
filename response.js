cf92bd9f0f459fc6a36bc9d4c018b8d94d865b51
KAVYA BHARATHI G <114849447+Kavyabharathi0117@users.noreply.github.com>
2023-12-25 10:31:32 +0530

    Add files via upload

---
+++ response.js
@@ -0,0 +1,8 @@
+const responseObj = {
+    Hello:"Hello! How can I assist you today?",
+    "How do I perform a transformer oil test":"1.Safety: Wear proper PPE for safety.Equipment 2.Check: Verify test equipment and calibration.Sample 3.Collection:Identify sampling point.Clean it thoroughly 4.Storage: Safely transport.6.Laboratory Analysis: Submit the sample to a lab for tests like interfacial tension.",
+       
+    "acceptable limits for insulation resistance during transformer testing":"The acceptable limits for insulation testing typically range from 1 to 10 megohms, depending on factors such as transformer type,and industry standards. However, precise limits should be determined based on the transformer's specifications and relevant guidelines.",
+      today: new Date().toDateString(),
+      time: new Date().toLocaleTimeString(),
+    };
